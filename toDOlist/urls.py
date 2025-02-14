50985aa324a40cfdff74f99e866d0bc8110109b3
ecourtced <cedric.ecourtemer@gmail.com>
2025-02-14 17:14:14 +0100

    Delete toDOlist directory

--- toDOlist/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-urlpatterns = [
-    path('',views.views_index,name="index"),
-    path('index/',views.views_index,name="index"),
-    path('blog/',views.blog,name="blog"),
-    #path('blog/',views.blog,name="blog"),
-]
-
